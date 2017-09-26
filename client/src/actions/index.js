1e08c7ff12781ce1e05816283f8ff142616fd51e
Michael Cruz <michaelscruz@yahoo.com>
2017-09-25 20:46:04 -0400

    Handle front end of stripe configuration

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,8 +5,13 @@ import { FETCH_USER } from "./types"
 // After the request completes, dispatch is called with the response as the
 // payload.
 export const fetchUser = () => async dispatch => {
-    // Using thunk and returning a function so that we can dispatch the action
-    // AFTER the request completes.
-    const res = await axios.get("/api/current_user")
-    dispatch({ type: FETCH_USER, payload: res.data })
-  }
+  // Using thunk and returning a function so that we can dispatch the action
+  // AFTER the request completes.
+  const res = await axios.get("/api/current_user")
+  dispatch({ type: FETCH_USER, payload: res.data })
+}
+
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post("/api/stripe", token)
+  dispatch({ type: FETCH_USER, payload: res.data })
+}
--- client/src/index.js
+++ client/src/index.js
@@ -14,4 +14,4 @@ const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
    document.querySelector("#root")
- )
+)
